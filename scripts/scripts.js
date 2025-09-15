fbed0e0ef44c09d03b815ec1aa7357f8840005ee
Mason Cordon <masoncordon@mail.weber.edu>
2025-09-14 20:05:14 -0600

    Added calculator function for triangles and pyramids, along with a new page to contain this function.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,34 @@ function LightMode(){
     document.getElementById('mainPage').style="background-color: white;";
     document.getElementById('condition').innerHTML = "Light Mode";
 }
+
+//Calculator math
+//Pyramid
+function pMath(){
+    let sideA = parseFloat(document.getElementById('pSideA').value);
+    let sideB = parseFloat(document.getElementById('pSideB').value);
+    let height = parseFloat(document.getElementById('pHeight').value);
+    console.log(sideA);
+    console.log(sideB);
+    console.log(height);
+
+    let volume = 1/3*(sideA * sideB * height);
+
+    document.getElementById('pVolume').innerHTML = volume;
+}
+
+//Triangle
+function tMath(){
+    let sideA = parseFloat(document.getElementById('tSideA').value);
+    let sideB = parseFloat(document.getElementById('tSideB').value);
+    let sideC = parseFloat(document.getElementById('tSideC').value);
+
+    let perimeter = parseFloat(sideA) + parseFloat(sideB) + parseFloat(sideC);
+    let semiperimeter = (parseFloat(sideA) + parseFloat(sideB) + parseFloat(sideC)) / 2;
+    let area = Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
+
+
+    document.getElementById('tPerimeter').innerHTML = perimeter;
+    document.getElementById('tSemiPerimeter').innerHTML = semiperimeter;
+    document.getElementById('tArea').innerHTML = area;
+}
