23ec9c4b22a0f215508e4a39b2f6e3fdbd7074f7
Mason Cordon <masoncordon@mail.weber.edu>
2025-10-06 00:29:25 -0600

    finished logic

--- scripts/memMatchscript.js
+++ scripts/memMatchscript.js
@@ -1,6 +1,5 @@
 let map = [];
 let shuffledArray = [];
-let indexCheck = 0;
 let tile1 = null, tile2 = null;
 
 const loadMap = (rows = 4, cols = 4) => {
@@ -20,7 +19,7 @@ const loadMap = (rows = 4, cols = 4) => {
     }    
 
     // copy and shuffle into `array'
-    shuffledArray = shuffle(array.slice());
+    shuffledArray = shuffle(array.slice(), rows, cols);
 
     console.log("Max value: " + maxValue);
     console.log("rows: " + rows);
@@ -30,7 +29,7 @@ const loadMap = (rows = 4, cols = 4) => {
     
 }
 
-const shuffle = (shuffledArray) => {
+const shuffle = (shuffledArray, rows, cols) => {
     //implementing Fisher-Yates shuffle
     for (let i = shuffledArray.length - 1; i > 0; i--){
         const j = Math.floor(Math.random() * (i + 1));
@@ -41,9 +40,24 @@ const shuffle = (shuffledArray) => {
 
     //check if it's actually shuffled
     console.log("Shuffled array: " + shuffledArray);
-    
+    sendTo2D(shuffledArray, rows, cols);
     return shuffledArray;
 
 }
 
-const sendTo2D = 
+const sendTo2D = (shuffledArray, rows, cols) => {
+    //transfer shuffled array to global variable as a 2D array
+    if (shuffledArray.length !== rows * cols) {
+        throw new Error('shuffledArray length does not equal rows * cols');
+    }
+    for (let i = 0; i < rows; i++){
+        map[i] = [];
+        for (let j = 0; j < cols; j++){
+            map[i][j] = shuffledArray[i * cols + j];
+        }
+        
+    }
+    // check if the array is actually 2D
+    console.log("2D array:", map);
+    return map;
+}
